8be3e42548648cb2b445084f66d3046baac66adf
claridiva2000 <claridiva2000@gmail.com>
2019-02-13 02:04:48 -0600

    drop nav done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // JS goes here
+
+const burger = document.querySelector('.enter');
+const menu = document.querySelector('.drop');
+const header = document.querySelector('h1');
+
+burger.addEventListener('click', ()=>{
+  drop.style.display="block";
+  burger.style.opacity = "0";
+  header.style.opacity = "0";
+
+
+})
+
+const expic = document.querySelector('.exit');
+const drop = document.querySelector('.drop');
+expic.addEventListener('click', () =>{
+ drop.style.display = "none";
+ burger.style.opacity = "1";
+ header.style.opacity = "1";
+});
