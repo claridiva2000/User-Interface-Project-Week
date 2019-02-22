5c5c2d1d37385b3fa3f4d732036146aec4b359c0
claridiva2000 <claridiva2000@gmail.com>
2019-02-22 01:06:07 -0600

    about us stretch almost done

--- js/index.js
+++ js/index.js
@@ -65,3 +65,59 @@ class TabContent {
 }
 
 const tabs = document.querySelectorAll('.tab').forEach(tab => new TabLink(tab));
+
+
+
+//Team
+
+const member1= document.querySelector(".team1");
+const pic1 = document.querySelector(".img1");
+
+member1.addEventListener("onmouseover", ()=>{
+  pic1.style.diplay="block";
+});
+
+
+
+// class TeamLink {
+//   constructor (tab) {
+//     // setting up a reference to our DOM node
+//     this.tab = tab;
+//     // set up a reference to our custom data attribute
+//     this.tabData = this.tab.dataset.tab;
+//     console.log(this.tabData);
+//     // sets up a reference to our matching tab-content's element using the data attribute
+//     this.tabContent = document.querySelector(`.card[data-tab="${this.tabData}"]`);
+//     console.log(this.tabContent);
+//     // creates a new version of Content as tabContent
+//     this.tabContent = new TabContent(this.tabContent);
+//     console.log(this.tabContent);
+//     // added a click event listener to our tab. That calls the method tabClick
+//     this.tab.addEventListener('click', this.tabClick.bind(this))
+//   }
+  
+//   // method
+//   tabHover() {
+//     // Using tabContent's toggleContent method we can toggle the classlist
+//     this.tabContent.toggleContent();
+//     document.querySelectorAll('.tab').forEach(button=>{
+//       button.classList.remove('active');
+//     })
+//     this.tab.classList.toggle('active');
+//   }
+// }
+
+// class TabContent {
+//   constructor (content) {
+//     this.content = content
+//   }
+//   // this method is what toggles the classlist
+//   toggleContent () {
+//     document.querySelectorAll('.card').forEach(content => {
+//      content.classList.remove('on')
+//     })
+//     this.content.classList.toggle('on')
+//   }
+// }
+
+// const people = document.querySelectorAll('.member').forEach(person => new TeamLink(person));
